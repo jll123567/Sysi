46b160a18594eb1f9befd07161ce60fd961940f7
jll123567 <ledbetterte@gmail.com>
2017-12-18 17:44:42 -0500

    Added uni-to-file save/load system
    to save your univers to a file run sysh.saves.saveUni(Object Universe, String filename) to generage a python script that has the name file name
    
    to load your uni import the file and run sysh.saves.<filename>.load() to get your univers back
    
    save.py makes saves in the saves folder of your install but you can add a directory path to file name to save it elsewhere
    
    Have a good day!

---
+++ saves/save.py
@@ -0,0 +1,12 @@
+# setup
+def saveUni(uni, fileName):
+    file = open(fileName+".py", 'w')
+    sameBegin = "# AUTO GENERATED CODE\nimport object\ndef load():\n    uni=object.universe("
+    variableMiddle = str(uni.tl)+","+str(uni.scn)+","+str(uni.obj)+","+str(uni.cont)+","+str(uni.funct)+","+str(uni.rule)+","+str(uni.tag)
+    sameEnd = ")\n    return uni\n# END AUTO GENERATED CODE\n# TO USE IMPORT THIS FILE AND RUN"+fileName+".load()"
+    file.write(sameBegin + variableMiddle + sameEnd)
+
+
+# runtime
+if __name__ == "__main__":
+    print("Save uni v10.0")
