ce5507bf9382a1bc731c3f5bc1ec4771f67ff2c8
jll123567 <ledbetterte@gmail.com>
2018-01-03 15:53:48 -0500

    added starndard queue and modified queue to match
    added a standard where a queue is devided into 6 parts
    status: tasks to check self status
    preserervation: tasks to maintain self
    limit: task to keep limiters in self.prs false
    command: tasks requested by others
    personal: taskes to make self.prs goals true
    aimless: what to do when there is nothing to do...

---
+++ standards_and_profiles/standardQueue.py
@@ -0,0 +1,35 @@
+# import
+import thread.memMgnt
+
+
+# setup
+# separates queue into 6 smaller queues that follow each other
+def addStdQueue(usr):
+    std = {"status": [], "preservation": [], "limit": [], "command": [], "personal": [], "aimless": []}
+    usr = thread.memMgnt.store(usr, 1, std)
+    return usr
+
+
+def makeValidQueue(stdQue):
+    mainQueue = []
+    for i in stdQue:
+        mainQueue.append(i)
+    return mainQueue
+
+
+def makeValidTskProfile(stdQue):
+    # flatten function by rightfootin
+    # snippet link: https://rightfootin.blogspot.com/2006/09/more-on-python-flatten.html
+        # modified to not include nested tuples as queues only support lists (or at least they're supposed to)
+    mainTask = []
+    for item in stdQue:
+        if isinstance(item, list):
+            mainTask.extend(makeValidTskProfile(item))
+        else:
+            mainTask.append(item)
+    return mainTask
+
+
+# runtime
+if __name__ == "__main__":
+    print("standard queue creator and converter v10.0")
