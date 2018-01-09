58999855c01ea3e5c0b69f53e6b563b04e8e664b
jll123567 <ledbetterte@gmail.com>
2018-01-08 21:19:01 -0500

    Added work in progress RPG Stats
    I'll do a better commit when i have time.

---
+++ standards_and_profiles/RPGStat.py
@@ -0,0 +1,10 @@
+# import
+# setup
+# adds some simple stats from most role playing games, because I am a nerd.
+# hp not needed as it part of wep
+# WIP
+def tags(obj):
+    obj.tag.update({"atk":0, "def":0, "agi":0, "lck":0})
+# runtime
+if __name__ == "__main__":
+    print("RPG stats v10.0")
