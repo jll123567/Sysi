9cb77c0e6d35fb79766fe04e4efa6fb1c916961e
jll123567 <ledbetterte@gmail.com>
2018-01-09 16:32:53 -0500

    Finished RPGStats
    adds methods to add some generic rpg stats to an objects tags
    
    addTag(obj)
    modifyTag(obj, int, int, int, int)
    removeTag(obj)

--- standards_and_profiles/RPGStat.py
+++ standards_and_profiles/RPGStat.py
@@ -1,10 +1,29 @@
-# import
 # setup
 # adds some simple stats from most role playing games, because I am a nerd.
 # hp not needed as it part of wep
-# WIP
-def tags(obj):
-    obj.tag.update({"atk":0, "def":0, "agi":0, "lck":0})
+
+
+def addTags(obj):
+    obj.tag.update({"atk": 0, "def": 0, "agi": 0, "lck": 0})
+    return obj
+
+
+def modifyTags(obj, atk, defence, agi, lck):
+    obj.tag["atk"] = atk
+    obj.tag["def"] = defence
+    obj.tag["agi"] = agi
+    obj.tag["lck"] = lck
+    return obj
+
+
+def removeTags(obj):
+    obj.tag.pop("atk")
+    obj.tag.pop("def")
+    obj.tag.pop("agi")
+    obj.tag.pop("lck")
+    return obj
+
+
 # runtime
 if __name__ == "__main__":
     print("RPG stats v10.0")
