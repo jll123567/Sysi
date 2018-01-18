c11ec1b012d415ab487048a1180050fa3d96a10e
jl123567 <ledbetterte@gmail.com>
2018-01-18 18:28:20 -0500

    Added dataTypes standard
    data in sysh is pretty genric so heres a way to state what kind of data is in dta.d
    
    just set the "dataType" tag to designate the type of data contined using the setType function or by literly setting it with dta.tag["dataType'] = <string>

---
+++ standards_and_profiles/dataTypes.py
@@ -0,0 +1,20 @@
+# setup
+def addTypeTags(dta):
+    dta.tag.update({"dataType": ""})
+    return dta
+
+
+# use the following or just do dta.tag["dataType"] = <string>
+def setType(dta, dataType):
+    dta.tag["dataType"] = str(dataType)
+    return dta
+
+
+def removeTypeTags(dta):
+    dta.tag.pop("dataType")
+    return dta
+
+
+# runtime
+if __name__ == "__main__":
+    print("data types tags v10.0")
