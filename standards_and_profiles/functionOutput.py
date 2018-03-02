65bcd035d7c5d4df850289e41a498663666dae4f
jl123567 <ledbetterte@gmail.com>
2018-03-02 17:04:29 -0500

    added out tag for future CContent Generation Engine

---
+++ standards_and_profiles/functionOutput.py
@@ -0,0 +1,29 @@
+# setup
+def addOutTags(obj):
+    obj.tag.update({"out": ""})
+    return obj
+
+
+# use the following or just do dta.tag["dataType"] = <string>
+def setOut(obj, output):
+    obj.tag["out"] = str(output)
+    return obj
+
+
+def removeOutTags(obj):
+    obj.tag.pop("dataType")
+    return obj
+
+
+# output tags give info as to what a fucntion will do as an array where each entry represents the output of a variable
+# outs for ints
+#   var = value
+#   var + value
+#   val - value
+#   val * value
+#   val / value
+#   val ?(used when you know the function modifies but not how
+
+# runtime
+if __name__ == "__main__":
+    print("output tags v10.0")
