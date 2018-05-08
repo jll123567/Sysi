69951a8c8b2d61d97bdbc28c43f523fa570d5823
jl123567 <ledbetterte@gmail.com>
2018-05-08 15:03:27 -0400

    added sockets implementation to transfer to make it easy to use with the internet     Fully documented as well

--- thread/transfer.py
+++ thread/transfer.py
@@ -15,34 +15,64 @@ def send(obj, sender, dta):
     return obj
 
 
-# Baised off: https://docs.python.org/3/howto/sockets.html
-def makeSocketInterface(interface)
+# socket code based off: https://docs.python.org/3/howto/sockets.html
 
-def connect(interface, host, port):
+# sets <obj>'s transfer interface to a socket
+# use <obj> = Sysh.thread.transfer.makeSocketInterface(<obj>)
+# requires obj with a transfer interface (<obj>.trd["transf"])
+def makeSocketInterface(obj):
+    obj.trd["transf"] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    return obj
+
+
+# connects the socket to <host> at <port>
+# use connectSocket(<interface>, <host>, <port>)
+# requires socket at <interface>
+def connectSocket(interface, host, port):
         interface.connect((host, port))
 
-def sendAscii(obj, msg):
+
+# sends an ascii encoded message though the socket
+# use sendSocket(<interface>, <message>)
+# requires socket at <interface>
+def sendSocket(interface, msg):
         msg = msg.encode("ascii")
         totalsent = 0
         while totalsent < len(msg):
-            sent = obj.sock.send(msg[totalsent:])
+            sent = interface.send(msg[totalsent:])
             if sent == 0:
                 raise RuntimeError("socket connection broken")
             totalsent = totalsent + sent
 
-def receive(obj):
+
+# receves 254 bytes from the socket
+# use connectSocket(<interface>)
+# requires socket at <interface>
+def receiveSocket(interface):
         chunks = []
         bytes_recd = 0
         while bytes_recd < 254:
-            chunk = obj.sock.recv(min(254 - bytes_recd, 2048))
+            chunk = interface.recv(min(254 - bytes_recd, 2048))
             if chunk == b'':
                 raise RuntimeError("socket connection broken")
             chunks.append(chunk)
             bytes_recd = bytes_recd + len(chunk)
         return b''.join(chunks)
 
-def disconnect(self):
-        self.sock.close()
+
+# closes the socket
+# use disconnectSocket(<interface>)
+# requires socket at <interface>
+def disconnectSocket(interface):
+        interface.close()
+
+
+# clears the interface
+# use obj = clearInterface(<obj>)
+# requires obj with transf interface
+def clearInterface(obj):
+    obj.trd["transf"] = None
+    return obj
 
 
 # runtime
