92aa9132dc57b76c0f761e6d21b9af5fb234a6eb
jl123567 <ledbetterte@gmail.com>
2018-05-08 14:39:49 -0400

    panic commit    nick no7

--- thread/transfer.py
+++ thread/transfer.py
@@ -1,3 +1,5 @@
+# import
+import socket
 # setup
 # transf
 # interface = container for receved data
@@ -11,6 +13,36 @@ def send(obj, sender, dta):
     pkg.tag.update({"sender": sender.tag["name"]})
     obj.trd["transf"] = pkg
     return obj
+
+
+# Baised off: https://docs.python.org/3/howto/sockets.html
+def makeSocketInterface(interface)
+
+def connect(interface, host, port):
+        interface.connect((host, port))
+
+def sendAscii(obj, msg):
+        msg = msg.encode("ascii")
+        totalsent = 0
+        while totalsent < len(msg):
+            sent = obj.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+def receive(obj):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < 254:
+            chunk = obj.sock.recv(min(254 - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+def disconnect(self):
+        self.sock.close()
 
 
 # runtime
