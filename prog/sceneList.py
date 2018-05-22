00a42a183503316d246db90c0412029f3d46e0f0
jl123567 <ledbetterte@gmail.com>
2018-05-22 15:06:10 -0400

    scene updated now sceneList in prog inherits from data o.f use posible now so so and err next?

---
+++ prog/sceneList.py
@@ -0,0 +1,49 @@
+# import
+import object
+
+
+# setup
+class sceneList(object.data):
+    def __init__(self, storage=None, tag={"name": None, "dataType": "sceneList"}):
+        super().__init__(storage, tag)
+
+    def loadScene(self, scn):
+        self.storage.append(scn)
+
+    def newScene(self, newScp=[], newObj=[],
+                 newLoc=object.container([None, 0, 0, 0], ["h,0,0,0-0,0,0"], {"name": "defaultContainer"}),
+                 newTag={"name": "emptyScene"}):
+        self.storage.append(object.scene(newScp, newObj, newLoc, newTag))
+
+    def loadObj(self, index, obj):
+        self.storage[index].obj.append(obj)
+
+    def unloadObj(self, sceneIndex, objIndex):
+        self.storage[sceneIndex].obj.pop(objIndex)
+
+    def switchScript(self, script, index):
+        self.storage[index].scp = script
+
+    def runScene(self, index):
+        count = 0
+        s = self.storage[index]
+        for i in s.scp:
+            if s.tag["paused"]:
+                print("paused")
+            else:
+                print(count, ":", i)
+                count += 1
+
+    def switchCont(self, index, cont):
+        self.storage[index].loc = cont
+
+    def pause(self):
+        if not self.tag["paused"]:
+            self.tag["paused"] = True
+        else:
+            self.tag["paused"] = False
+
+
+# runtime
+if __name__ == "__main__":
+    print(" scene handle v10.0")
