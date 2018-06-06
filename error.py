6af7178bc496cbbc3e1522cde4a13d4308684fbb
jl123567 <ledbetterte@gmail.com>
2018-06-06 15:02:53 -0400

    error is up to date

--- error.py
+++ error.py
@@ -9,43 +9,37 @@ import object
 # message: string to describe the issue
 # resolutions: what can be done to fix the issue
 # selected: an integar that is the index of the resolution you want(None if no resolution selected) ex: want b [a,b,c,d] selected:1
+class err(object.data):
+    def __init__(self, errType, severity, message, resolutions, selected, tag):
+        self.errDta = {"code": (str(errType) + str(severity) + ":" + message),
+                       "resolutions": resolutions,
+                       "selected": selected}
+        self.tag = tag
 
-def createError(errType, severity, message, resolutions, selected, name):
-    e = object.storageata(
-        {"code": (str(errType) + str(severity) + ":" + message), "resolutions": resolutions, "selected": selected},
-        {"name": name, "relevancy": [0, 0, 0], "dataType": "err"})
-    return e
+    def setError(self, errType, severity, message, resolutions, selected):
+        self.errDta["code"](str(errType) + str(severity) + ":" + message)
+        self.errDta["resolutions"] = resolutions
+        self.errDta["selected"] = selected
 
+    def clearError(self):
+        self.errDta = None
 
-def setError(err, errType, severity, message, resolutions, selected):
-    err.storage["code"] = (str(errType) + str(severity) + ":" + message)
-    err.storage["resolutions"] = resolutions
-    err.storage["selected"] = selected
-    return err
-
-
-def clearError(err):
-    err.storage = None
-    err.tag["dataType"] = "none"
-
-
-def resolveError(err):
-    resolveing = True
-    print(err.storage["code"])
-    count = 0
-    for i in err.storage["resolutions"]:
-        print(str(count) + ":" + i)
-        count += 1
-    while resolveing:
-        try:
-            selected = input("resolution?:")
-            err.storage["selected"] = int(selected)
-        except ValueError:
-            print("int only")
-        else:
-            resolveing = False
-    print(err.storage, err.tag)
-    return err
+    def resolveError(self):
+        resolveing = True
+        print(self.errDta["code"])
+        count = 0
+        for i in self.errDta["resolutions"]:
+            print(str(count) + ":" + i)
+            count += 1
+        while resolveing:
+            try:
+                selected = input("resolution?:")
+                self.errDta["selected"] = int(selected)
+            except ValueError:
+                print("int only")
+            else:
+                resolveing = False
+        print(self.errDta, self.tag)
 
 
 # Runtime
