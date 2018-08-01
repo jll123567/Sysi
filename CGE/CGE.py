4d050e4831d46b4e0e863fcdb9a773c655576032
jl123567 <ledbetterte@gmail.com>
2018-08-01 13:31:30 -0400

    made functions to get the atributes and  methods of objects

--- CGE/CGE.py
+++ CGE/CGE.py
@@ -1,6 +1,40 @@
-#task > CGE
-#["target(obj name)", "operation", [paramaters]]
+import re
 
-#CGE > sceneScript
-#["sender","traget","oepration",[paramaters]]
 
+# task > CGE
+# ["target(obj name)", "operation", [paramaters]]
+
+# CGE > sceneScript
+# ["sender","traget","oepration",[paramaters]]
+
+
+# noinspection PyPep8Naming
+def getAttribs(obj):
+    objDict = str(obj.__dict__.keys())
+    stringList = str(re.search(r"'.*'", objDict).group())
+    words = []
+    word = ''
+    for char in stringList:
+        if char == '\'' or char == "\"" or char == ' ':
+            continue
+        if char == ',':
+            words.append(word)
+            word = ''
+        else:
+            word += char
+    words.append(word)
+    return words
+
+
+# noinspection PyPep8Naming
+def getMethods(obj, atribsList):
+    # noinspection PyPep8Naming
+    methodList = dir(obj)
+    finalList = []
+    for method in methodList:
+        if method[0] == '_':
+            continue
+        if method in atribsList:
+            continue
+        finalList.append(method)
+    return finalList
