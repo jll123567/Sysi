a654403d0accc30993d745f45c618a551e1be64f
jl123567 <ledbetterte@gmail.com>
2018-09-25 17:43:26 -0400

    doc1 for real this time

--- atribs/personality.py
+++ atribs/personality.py
@@ -1,8 +1,8 @@
-# Description of the usr.prs atribute
+# Description of the usr.prs attribute
 # Module type:def
 
 
-# user peronality(general behavior guide) Goals([] of boolean expressions that define goals)*, limits([] of boolean
+# user personality(general behavior guide) Goals([] of boolean expressions that define goals)*, limits([] of boolean
 # expressions that define limits(goals to avoid))*, functions([] of available functions)*
 # note that order of goals/ limits determines importance(goal[0] is more important that goal[1])
 class prs:
@@ -21,38 +21,57 @@ class prs:
             self.functions = functions
 
     # sets the values of limit, goal and funct
-    # limit([] of limits)*, goal([] of goals)*, funct([] of functions availiable)
+    # limit([] of limits)*, goal([] of goals)*, funct([] of functions available)
     def setPrs(self, limit, goal, funct):
         self.limits = limit
         self.goals = goal
         self.functions = funct
 
     # adds a new goal at goals[index]
-    # k
+    # goal(goal)*, index(int)*
+    # No output
     def newGoal(self, goal, index):
         self.goals.insert(index, goal)
 
+    # adds a new limit at limits[index]
+    # limit(limit)*, index(int)*
+    # No output
     def newLimit(self, limit, index):
         self.limits.insert(index, limit)
 
+    # adds a new available function to functions
+    # funct(available function)*
+    # No output
     def newFunction(self, funct):
         self.functions.append(funct)
 
+    # removes a goal at index
+    # index(int)*
+    # No output
     def removeGoal(self, index):
         self.goals.pop(index)
 
+    # removes a limit at index
+    # index(int)*
+    # No output
     def removeLimit(self, index):
         self.limits.pop(index)
 
+    # removes a available function at index
+    # index(int)*
+    # No output
     def removeFunction(self, index):
         self.functions.pop(index)
 
+    # sets all prs attributes to None
+    # No input
+    # No output
     def clearPrs(self):
         self.limits = None
         self.goals = None
         self.functions = None
 
 
-# runtime
+# info at run
 if __name__ == "__main__":
-    print("pers def and functions v11.0")
+    print("Description of the usr.prs attribute\nModule type:def")
