8e776c8546bcfdf7334d1cd4145d3674293e2986
jl123567 <ledbetterte@gmail.com>
2018-10-26 20:02:25 -0400

    mhm test was beautiful
    Signed-off-by: jl123567 <ledbetterte@gmail.com>

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import object
 import CGE
-import prog.idGen as idGen
+# import prog.idGen as idGen
 
 a = object.object()
 b = object.object()
 sc = object.scene()
 sc.obj = [a, b]
-for idx in range(0, 1):
-    sc.obj[idx].tag["id"] = idGen.generateGenericId(sc.obj, sc.obj[idx])
-sc.scp = [None, [[sc.obj[0].tag["id"]+".trd.tsk", "debugPrint", ["sh0 op0"]], [sc.obj[0].tag["id"]+".trd.tsk", "debugPrint", ["sh0 op1"]]], [[sc.obj[0].tag["id"]+".trd.tsk", "debugPrint", ["sh1 op0"]], [sc.obj[0].tag["id"]+".trd.tsk", "debugPrint", ["sh1 op1"]]]]
+# for idx in range(0, 1): sc.obj[idx].tag["id"] = idGen.generateGenericId(sc.obj, sc.obj[idx]) sc.scp = [None,
+# [[sc.obj[0].tag["id"]+".trd.tsk", "debugPrint", ["sh0 op0"]], [sc.obj[0].tag["id"]+".trd.tsk", "debugPrint",
+# ["sh0 op1"]]], [[sc.obj[0].tag["id"]+".trd.tsk", "debugPrint", ["sh1 op0"]], [sc.obj[0].tag["id"]+".trd.tsk",
+# "debugPrint", ["sh1 op1"]]]]
 CGE.replayScene(sc, 3)
