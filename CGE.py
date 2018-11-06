8ebf37f37388183e099b43043286e665ca748b08
jl123567 <ledbetterte@gmail.com>
2018-11-06 14:43:19 -0500

    added a todo
    Signed-off-by: jl123567 <ledbetterte@gmail.com>

--- CGE.py
+++ CGE.py
@@ -1,6 +1,7 @@
 # The Content Generation engine
 # objects in objList are simulated and run based on the instructions in trd.tsk
 # Module type: prog
+# todo add multiple sessions and universe rule handling
 # task > CGE
 # ["target(obj name)", "operation", [parameters]]
 # CGE > sceneScript
