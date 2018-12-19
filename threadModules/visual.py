49550a716cbee1d5273f4907ae537aabdcff7687
jl123567 <ledbetterte@gmail.com>
2018-12-19 14:37:23 -0500

    added docs can't do more now due to dieing battery
    Signed-off-by: jl123567 <ledbetterte@gmail.com>

--- threadModules/visual.py
+++ threadModules/visual.py
@@ -1,13 +1,19 @@
-# visual data and camera handling
-# module type: def
-
-
+"""visual data and camera handling"""
 import object
-# feed=[raw,pitch,yaw,roll]
 
 
 class vis:
+    """hold and process visual data
+
+    rawImg is a list with images(images will be added later)
+    rx is float representing degrees
+    so are ry and rz"""
     def __init__(self, rawImg=None, pitch=0, yaw=0, roll=0):
+        """initialize object attributes
+        rawImg is an empty list
+        pitch is 0
+        yaw is 0
+        roll is 0"""
         if rawImg is None:
             self.rawImg = []
         else:
@@ -16,10 +22,8 @@ class vis:
         self.ry = yaw
         self.rz = roll
 
-    # set camera rotation
-    # p(float)*, y(float)*, r(float)*
-    # none
     def rotate(self, rx, ry, rz):
+        """set the rotation attributes of the visual thread"""
         self.rx = rx
         self.ry = ry
         self.rz = rz
@@ -28,12 +32,14 @@ class vis:
     # none
     # none
     def clearImg(self):
+        """set the rawImg attribute to an empty list"""
         self.rawImg = []
 
     # reset position of camera
     # none
     # none
     def resetPos(self):
+        """set rotation attributes to 0"""
         self.rx = 0
         self.ry = 0
         self.rz = 0
@@ -42,6 +48,7 @@ class vis:
     # none
     # dta(vis attribs, tags)
     def package(self):
+        """pack attributes into a data object and return it"""
         return object.data([self.rawImg, self.rx, self.ry, self.rz], {"name": "tread.vis.package", "id": None,
                                                                       "dataType": "thread.vis.package"})
 
