89b68fa15abba1cdb05948d087d2b8e6459043c0
jll123567 <ledbetterte@gmail.com>
2019-03-05 14:44:29 -0500

    set pan in language to use -1.0 to 1.0 added proper(ish) docstrings
    Signed-off-by: jll123567 <ledbetterte@gmail.com>

--- threadModules/language.py
+++ threadModules/language.py
@@ -1,14 +1,10 @@
-# lang handling
-# module type: def
-# feed=[in,out]
-# in=[[amplitude,amplitude,amplitude],[Right version of sound]] each index is one mS
-# out=[amplitude,amplitude,amplitude](mono)
+"""lang handling"""
 import object
 
 
-# stereo audio for lang
 # left input([int]), right input([int])
 class audioStereo:
+    """stereo audio"""
     def __init__(self, left=None, right=None):
         if left is None:
             self.left = []
@@ -20,9 +16,9 @@ class audioStereo:
             self.right = right
 
 
-# mono audio
 # sound input([int])
 class audioMono:
+    """mono audio"""
     def __init__(self, sound=None):
         if sound is None:
             self.sound = []
@@ -30,9 +26,9 @@ class audioMono:
             self.sound = sound
 
 
-# language thread module
 # heard audio(audioStereo), spoken(audioMono)
 class lang:
+    """language thread module class"""
     def __init__(self, heard=audioStereo(), speakQue=audioMono()):
         self.heard = heard
         self.speakQue = speakQue
@@ -41,56 +37,45 @@ class lang:
     # inputSource(audioStereo)*
     # none
     def listen(self, inputSource):
-        listening = 0
-        while listening < 1000:
-            self.heard.left.append(inputSource.l)
-            self.heard.right.append(inputSource.r)
-            listening += 1
+        """gets sound from input and appends it to heard"""
+        self.heard.left.append(inputSource.l)
+        self.heard.right.append(inputSource.r)
 
-    # tunes based on direction and minimum volume as an int
-    # min Volume to reeve(int)*, minimum panning(int -100(l) to 100(r))*, maximum panning(int -100(l) to 100(r))*
-    # todo make pan -1 to 1 float
     def tune(self, minVolume, minPan, maxPan):
+        """check if audio is above a minimum volume or withing a pan range and if its not cut it
+        min volume is in Db
+        pan is a float range
+        minPan is the smallest pan value(up to -1.0)
+        maxPan is the largest pan value(up to 1.0)"""
         for i in self.heard.left:
             if abs(i) < minVolume:
                 self.heard.left[self.heard.left.index(i)] = 0
             elif minPan > 0:
                 self.heard.left[self.heard.left.index(i)] = 0
-            elif abs(i) > minPan * (-2.2):
+            elif abs(i) > minPan * -100:
                 self.heard.left[self.heard.left.index(i)] = 0
             else:
                 continue
 
         for i in self.heard.right:
             if abs(i) < minVolume:
-                self.heard.right[self.heard.left.index(i)] = 0
+                self.heard.right[self.heard.right.index(i)] = 0
             elif maxPan < 0:
-                self.heard.left[self.heard.left.index(i)] = 0
-            elif abs(i) < maxPan * 2.2:
-                self.heard.left[self.heard.left.index(i)] = 0
+                self.heard.right[self.heard.right.index(i)] = 0
+            elif abs(i) < maxPan * 100:
+                self.heard.right[self.heard.right.index(i)] = 0
             else:
                 continue
 
-    # clears spoken audio
-    # none
-    # none
     def silence(self):
+        """clear spoken audio"""
         self.speakQue = []
 
-    # queue <sounds> to obj out
-    # sounds(audioMono)*
-    # none
     def queueSpeak(self, sounds):
+        """add sounds to the speaking queue"""
         self.speakQue = sounds
 
-    # pack data for ram
-    # none
-    # dta(lang attribs, tags)
     def package(self):
+        """pack audio data into a data obj to send to ram and return it"""
         return object.data([self.heard, self.speakQue], {"name": "tread.lang.package", "id": None,
                                                                  "dataType": "thread.lang.package"})
-
-
-# info at run
-if __name__ == "__main__":
-    print("lang handling\nmodule type: def")
