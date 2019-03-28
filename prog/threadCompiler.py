1c91e466de09e76a55f9b12c2df1e457e26b6958
jll123567 <ledbetterte@gmail.com>
2019-03-28 14:56:52 -0400

    added noted for threadcode compiliation
    Signed-off-by: jll123567 <ledbetterte@gmail.com>

---
+++ prog/threadCompiler.py
@@ -0,0 +1,51 @@
+# OPERATION{
+#     Target, Method, Prams, Source
+# }
+# SHIFT{OPERATION{}, ...}
+# PROFILE{SHIFT{}, ...}
+# CURRENT{SHIFT{}}
+#
+# whitespace is not parsed accept for in Prams.
+# Keywords have open and close brackets.
+# Lists of elements are comma delimited.
+# OPERATION
+#     Keyword for an operation, requires four elements  comma seperated.
+#     Target: the target for the operation, converted to a string when compiled(ex. a -> "a")
+#     Method: the method the target will execute, converted to a string when compiled(ex. a -> "a")
+#     Prams: Paramaters for the operation, coppied unformatted
+#         Prams must be a python list.(ex. [a, b, c, d])
+#         for methods with no paramaters supply an empty list(ex. [])
+#     Source: the object that made the request, converted to a string when compiled(ex. a -> "a")
+#
+# SHIFT
+#     Keyword for a shift.
+#     Only acepts OPERATIONs.
+#     OPERATIONs must be comma delimited
+#
+# PROFILE
+#     Keyword for a thread_modules.tsk profile.
+#     Only one PROFILE may exist in a file
+#     Only accepts SHIFTs, comma delimited.
+#     If PROFILE in in the file, compiler will output a thread_modules.tsk.
+#
+# CURRENT
+#     Keyword for a thread_modules.tsk current.
+#     Only one CURRENT may exist in a file
+#     Only accepts a single SHIFT.
+#     If CURRENT in in the file, compiler will output a thread_modules.tsk.
+#
+# basic example
+#
+# CURRENT{
+#     SHIFT{
+#         OPERATION{
+#             a,
+#             print,
+#             ["Hello, World!"],
+#             a,
+#         }
+#     }
+# }
+#
+# compact
+# CURRENT{SHIFT{OPERATION{a,print,["Hello, World!"],a,}}}
