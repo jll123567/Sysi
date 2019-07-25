c696f38af81821a9ca0e984ee27f29cf5f5c797f
jl123567 <ledbetterte@gmail.com>
2019-07-25 17:53:12 -0400

    Made sysSock to handle all the sending/receiving stuff. Modified sysClient and sysServer so they better reflect what they'll be doing in the future. Made sysSock be able to send rawObjects using pickle.dumps() .     Note it wont work with threading.Thread or _thread.lock objects.
    Signed-off-by: jll123567 <ledbetterte@gmail.com>

--- clientServer.py
+++ clientServer.py
@@ -1,69 +1,119 @@
+import pickle
+import re
 import socket
 import threading
-from time import sleep
+
+
+# from time import sleep
+
+# cli sShift rObj
+# serv sObj rShift
+#     runs dirs
+#     keep track of clients
+#
+
+class sysSock(threading.Thread):
+    """
+    Handle data from individual sockets.
+        All data should be either bytes or ascii strings.
+
+    address is the hostname or ip address to connect/listen.
+    port is the port to connect/listen.
+    isSending is false for receiving and true for sending.
+    data: when sending(self.isSending=True) this is the data sent.
+        When receiving self.data should be what was received.
+    dataType is a string to describe how to handle data.
+    sock is the actual socket
+    """
+
+    def __init__(self, address, port, isSending=False, data=None, dataType=None, sock=None):
+        """
+        :param address: str
+        :param port: int
+        :param isSending: bool
+        :param data: any
+        :param dataType: string
+        :param sock: socket.socket
+        """
+        super().__init__()
+        self.sock = sock
+        self.address = address
+        self.port = port
+        self.isSending = isSending
+        if not self.isSending:
+            self.data = None
+        else:
+            self.data = data
+        if dataType is None:
+            self.dataType = "unspecified"
+        else:
+            self.dataType = dataType
+
+    def run(self):
+        if self.isSending:
+            self.sock = socket.socket()
+            self.sock.connect((self.address, self.port))
+            toSend = None
+            if self.dataType == "obj":
+                pickledData = self.data
+                pickledData = pickle.dumps(pickledData)
+                toSend = "obj" + ':' + pickledData.hex()
+                toSend = toSend.encode("ascii")
+            self.sock.sendall(toSend)
+            self.sock.close()
+        else:
+            self.sock = socket.socket()
+            self.sock.bind((self.address, self.port))
+            self.sock.listen(1)
+            final = b""
+            while True:
+                connection, clientAddress = self.sock.accept()
+                try:
+                    while True:
+                        partial = connection.recv(128)
+                        if partial:
+                            final += partial
+                        else:
+                            break
+                finally:
+                    connection.close()
+                    self.sock.close()
+                    break
+            pair = re.split(r":", final.decode("ascii"))
+            self.dataType = pair[0]
+            self.data = pair[1]
+            if self.dataType == "obj":
+                self.data = pickle.loads(bytearray.fromhex(self.data))
 
 
 class sysClient(threading.Thread):
-    def __init__(self, message, sAddress, sPort, rAddress, rPort, sSock=None, rSock=None, rStor=None, sStor=None):
+    def __init__(self, clientId, obj=None, nextShift=None):
         super().__init__()
-        self.message = message
-        self.sAddress = sAddress
-        self.sPort = sPort
-        self.rAddress = rAddress
-        self.rPort = rPort
-        self.rStor = rStor
-        self.sStor = sStor
-        if sSock is None:
-            self.sSock = socket.socket()
+        self.clientId = clientId
+        self.obj = obj
+        if nextShift is None:
+            self.nextShift = []
         else:
-            self.sSock = sSock
-        if rSock is None:
-            self.rSock = socket.socket()
-        else:
-            self.rSock = rSock
+            self.nextShift = nextShift
+        self.sSock = None
+        self.rSock = None
 
     def run(self):
-        print(self.sendAscii(self.message))
-        print(self.receiveAscii())
+        pass
 
-    def receiveAscii(self):
-        self.rSock.bind((self.rAddress, self.rPort))
-        self.rSock.listen(1)
-        while True:
-            connection, client_address = self.rSock.accept()
-            final = b""
-            try:
-                while True:
-                    partial = connection.recv(16)
-                    if partial:
-                        final += partial
-                    else:
-                        print(final.decode("ascii"))
-                        break
-            finally:
-                self.rStor = final.decode("ascii")
-                connection.close()
-                break
-        self.rSock.close()
-        return "rSock closed on client"
-
-    def sendAscii(self, msg):
-        self.sSock.connect((self.sAddress, self.sPort))
-        self.sSock.sendall(msg.encode("ascii"))
-        sleep(5)
-        self.sSock.close()
-        return "sSock closed on client"
+    # sendShift(serverAddress, shift, objId)
+    # dispObj(obj)
+    # readReceive()
 
 
 class sysServer(threading.Thread):
-    def __init__(self, sAddress, sPort, rAddress, rPort, sSock=None, rSock=None, rStor=None, sStor=None):
+    """
+    Runs directories, manages client connections.
+    """
+
+    def __init__(self, sSock=None, rSock=None, dirs=None, clientIdLookupTable=None):
         super().__init__()
-        self.sAddress = sAddress
-        self.sPort = sPort
-        self.rAddress = rAddress
-        self.rPort = rPort
-        self.rStor = rStor
-        self.sStor = sStor
+
         if sSock is None:
             self.sSock = socket.socket()
         else:
@@ -72,45 +122,28 @@ class sysServer(threading.Thread):
             self.rSock = socket.socket()
         else:
             self.rSock = rSock
+        self.dirs = dirs
+        if clientIdLookupTable is None:
+            self.clientIdLookupTable = {}
+        else:
+            self.clientIdLookupTable = clientIdLookupTable
 
     def run(self):
-        print(self.receiveAscii())
-        sleep(1)
-        print(self.sendAscii(self.rStor))
-
-    def sendAscii(self, msg):
-        self.sSock.connect((self.sAddress, self.sPort))
-        self.sSock.sendall(msg.encode("ascii"))
-        sleep(1)
-        self.sSock.close()
-        return "sSock closed on client"
-
-    def receiveAscii(self):
-        self.rSock.bind((self.rAddress, self.rPort))
-        self.rSock.listen(1)
-        while True:
-            connection, client_address = self.rSock.accept()
-            final = b""
-            try:
-                while True:
-                    partial = connection.recv(16)
-                    if partial:
-                        final += partial
-                    else:
-                        print(final.decode("ascii"))
-                        break
-            finally:
-                connection.close()
-                self.rStor = final.decode("ascii")
-            break
-        self.rSock.close()
-        return "rSock closed on server"
+        pass
+    # giveShift(objId,shift)
+    # sendObj(obj,clientId)
+    # readReceive()
 
 
-srv = sysServer("localhost", 9998, "localhost", 9999)
-cli = sysClient("hello!", "localhost", 9999, "localhost", 9998)
-srv.start()
-cli.start()
-sleep(10)
-del srv
-del cli
+if __name__ == "__main__":
+    class c:
+        def __init__(self, stor):
+            self.stor = stor
+
+        def read(self):
+            return self.stor
+
+    cli = sysSock("localhost", 9999, True, c("yeet"), "obj")
+    srv = sysSock("localhost", 9999)
+    srv.start()
+    cli.start()
--- test.py
+++ test.py
@@ -1,45 +1,23 @@
 """don't mind me, just being dumb"""
-import sys_objects
-import CGE
-import thread_modules
+import pickle
+import re
 
 
-class sendableObj(sys_objects.sysObject):
-    def __init__(self, m=None, tr=None, tg=None):
-        super().__init__(m, tr, tg)
-        self.trd.transf = thread_modules.Transfer()
+class c:
+    def __init__(self, stor):
+        self.stor = stor
 
-    def generateSendRequest(self, dta, targetId):
-        self.trd.transf.send(self.tag["id"], dta)
-        request = [targetId, "acceptSendRequest", [self.trd.transf.interface], self.tag["id"]]
-        self.trd.tsk.current.append(request)
-
-    def acceptSendRequest(self, dta):
-        print("aSR started")
-        willReceve = False
-        if dta.tag["sender"] is not None:
-            willReceve = True
-        if willReceve:
-            acceptOp = [self.tag["id"], "receiveSendData", [dta], self.tag["id"]]
-            self.trd.tsk.addOperation(acceptOp)
-        print("aSR Finished")
-
-    def receiveSendData(self, dta):
-        self.trd.transf.receive(dta)
-
-    def debugInterface(self):
-        print(self.trd.transf.interface.storage)
+    def read(self):
+        return self.stor
 
 
-b = sendableObj()
-d = sendableObj()
-dtaA = sys_objects.data("Hi")
-dtaB = sys_objects.data("OverwriteMe")
-b.tag["id"] = "o/b"
-d.tag["id"] = "o/d"
-d.trd.transf.interface = dtaB
-b.generateSendRequest(dtaA, "o/d")
-d.trd.tsk.current.append(["o/d.trd.tsk", "loopInf", [["o/d", "debugInterface", [], "o/d"]], "o/d"])
-# d.trd.tsk.profile = [[]]
-session = CGE.CGESession("f", [b, d], ["c", False])
-session.start()
+a = c("yeet")
+print(a)
+b = pickle.dumps(a)
+print(b)
+d = "obj:"+b.hex()
+print(d)
+e = None
+if re.match(r"obj:.*", d):
+    e = pickle.loads(bytearray.fromhex(re.split(r":", d)[1]))
+    print(e, e.read())
