fba341d27702a3d3d7ec7cced9d74bd29cbff658
Jacob Ledbetter <ledbetterte@gmail.com>
2019-10-13 15:11:53 -0400

    Did some practice with tk. Going to do a tutorial to learn all the widgets. Wish me luck.
    Signed-off-by: Jacob Ledbetter <ledbetterte@gmail.com>

--- test.py
+++ test.py
@@ -3,18 +3,10 @@ Don't mind me, just being testing thisngs here.
 Ideally this file will be removed in any sort of "real release".
 """
 import sys_objects
-import thread_modules
-
+import CGE
 a = sys_objects.sysObject()
-a.tag["id"] = "un/tst/o/00"
-a.trd.sub = thread_modules.SubObjManager()
-a.trd.mov = thread_modules.Move()
-a.trd.mov.warp(1, 2, 1)
-b = sys_objects.sysObject()
-b.tag["id"] = "un/tst/o/11"
-b.trd.sub = thread_modules.SubObjManager()
-b.trd.mov = thread_modules.Move()
-b.trd.mov.warp(5, 5, 5)
-a.trd.sub.addChild(b)
-b.addParent(a)
-print(b.getActualChildMov().debugMov())
+a.tag['id'] = "un/o/0"
+a.blankTask()
+a.trd.tsk.appendCurrent(["un/o/0", "sayHi", [], "un/o/0"])
+ses = CGE.CGESession("un/0", [a], ['c', False])
+ses.start()
