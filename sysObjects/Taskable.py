b41cb5ad8bb6458c569000229965b935ccfeb432
Jacob Ledbetter <ledbetterte@gmail.com>
2020-05-12 14:26:55 -0400

    Added the ability for taskable object to dynamically make and use functions from strings.
    Added makeFunctions.
    Added attachFunction.
    Added bindFunction.
    Added installFunctionSuite.
    Doc-ed and commented for your convenience.
    
    Signed-off-by: Jacob Ledbetter <ledbetterte@gmail.com>

--- sysObjects/Taskable.py
+++ sysObjects/Taskable.py
@@ -6,6 +6,7 @@ Classes
 """
 from sysObjects.Tagable import Tagable
 from sysModules.Tasker import Tasker
+import types
 
 
 class Taskable(Tagable):
@@ -33,4 +34,87 @@ class Taskable(Tagable):
         """
         super().__init__(tags)
         self.tasker = tsk
-# TODO: Implement Dynamic function stuff.
+
+    @staticmethod
+    def makeFunctions(code_string):
+        """
+        Take a string with only function definitions and return a list of functions.
+
+        If only one function is made it is returned alone rather than in a list.
+
+        Parameters
+            code_string str: A string with a set of functions.
+
+        Return
+            [function]/function: The functions generated from <code_string>.
+        """
+        code = compile(code_string, "<Taskable>", "exec")  # Compile code string to get a code object.
+
+        fCs = []
+        fNs = []
+        for i in code.co_consts:  # Get and separate the function's code objects and the function's names.
+            if isinstance(i, str):
+                fNs.append(i)
+            else:
+                fCs.append(i)
+        fCs.pop(-1)  # Get rid of return constant(None).
+
+        fUs = []
+        for i in range(fCs.__len__()):  # Make the functions from the code objects.
+            fUs.append(
+                types.FunctionType(fCs[i], globals(), fNs[i]))  # Globals is needed to access global vars/ builtins.
+
+        if fUs.__len__() == 1:  # Return
+            return fUs[0]
+        else:
+            return fUs
+
+    def attachFunction(self, funct, attr):
+        """Set <funct> to self.<attr>."""
+        setattr(self, attr, funct)
+
+    def bindFunction(self, fuAttr):
+        """Make the function in self.<fuAttr> a bound method."""
+        mtd = types.MethodType(getattr(self, fuAttr), self)  # Make a bound method from the function at <fuAttr>.
+        setattr(self, fuAttr, mtd)  # Reattach
+
+    def installFunctionSuite(self, code_string):
+        """
+        Make, attach, and possibly bind a set of functions from <code_string>.
+
+        All functions will be attached but some will be made into bound methods if their first argument is named 'self'.
+        Functions/methods are attached with their name. Existing attributes with the same name will be OVERRIDDEN!!!
+        <code_string> must follow all the same rules for makeFunctions.
+
+        Parameters
+            code_string str: A string with a set of functions.
+        """
+        code = compile(code_string, "<Taskable>", "exec")  # Same as in makeFunctions
+        fCs = []
+        fNs = []
+        for i in code.co_consts:
+            if isinstance(i, str):
+                fNs.append(i)
+            else:
+                fCs.append(i)
+        fCs.pop(-1)
+
+        mthds = []
+        functs = []  # Separate the functions from the methods.
+        for i in range(fCs.__len__())[::-1]:  # Parse in reverse because popping.
+            if fCs[i].co_varnames.__len__() == 0:  # Handle zero args/vars.
+                functs.append([fCs.pop(i), fNs.pop(i)])
+            elif fCs[i].co_varnames[0] == "self":  # Its a method if the first varname(ie argument) is "self".
+                mthds.append([fCs.pop(i), fNs.pop(i)])
+            else:  # Some args/vars but no self.
+                functs.append([fCs.pop(i), fNs.pop(i)])
+
+        for i in functs:  # For functions: attach to self with name.
+            f = i[0]
+            n = i[1]
+            self.attachFunction(types.FunctionType(f, globals(), n), n)  # Globals to keep global vars/builtins.
+        for i in mthds:  # For methods: attach to self with name and bind.
+            f = i[0]
+            n = i[1]
+            self.attachFunction(types.FunctionType(f, globals(), n), n)
+            self.bindFunction(n)
