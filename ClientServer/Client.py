323736d34e48ed842bc3397811104bf084d85e5c
Jacob Ledbetter <ledbetterte@gmail.com>
2020-07-09 10:24:49 -0400

    Server and Client are finished.
    They now talk to eachother correctly and have ways to handle disconnects.
    
    Todo: documentation and comments.
    Signed-off-by: Jacob Ledbetter <ledbetterte@gmail.com>

--- ClientServer/Client.py
+++ ClientServer/Client.py
@@ -10,7 +10,7 @@ import asyncio
 import base64
 import pickle
 import socket
-
+import time
 
 class SysClient(threading.Thread, sysObjects.Tagable.Tagable):
     """
@@ -28,9 +28,13 @@ class SysClient(threading.Thread, sysObjects.Tagable.Tagable):
             self.sessionList = []
         else:
             self.directoryList = ses
+        if shifts is None:
+            self.shiftQueue = []
+        else:
+            self.shiftQueue = shifts
         self.server = server  # (address, port)
-        self.obj = obj  # (dir, ses, obj)
-        self.shiftQueue = shifts
+        self.obj = obj  # [dir, ses, obj]
+
         self.address = socket.gethostbyname(socket.gethostname())
         self.port = port
         self.live = True
@@ -45,32 +49,12 @@ class SysClient(threading.Thread, sysObjects.Tagable.Tagable):
     def run(self):
         asyncio.run(self.main())
 
+
     async def main(self):
         try:
             await asyncio.gather(self.connectToServer(), self.taskManager())
         except asyncio.CancelledError:
             print("Tasks canceled. Live: {}".format(self.live))  # debug
-
-    async def handleServerRequests(self, reader):
-        pass  # TODO: Make me work lol.
-
-    async def sendShifts(self, writer):
-        while self.live:
-            if self.shiftQueue:  # Only try to send a shift if there are any to send.
-                shift, idx = self.shiftQueue.pop()
-                shiftDt = base64.b64encode(pickle.dumps(shift))
-                idxDt = base64.b64encode(str(idx).encode())
-                objIdDt = base64.b64encode(self.obj.tags["id"].encode())
-                writer.write(b"shift:" + objIdDt + b":" + shiftDt + b":" + idxDt + b":end")
-                try:
-                    await writer.drain()
-                except ConnectionResetError:
-                    # print("connection reset. ending...")  # debug
-                    return
-                writer.close()
-                await writer.wait_closed()
-            else:
-                await asyncio.sleep(0)
 
     async def taskManager(self):
         while self.live:
@@ -85,11 +69,15 @@ class SysClient(threading.Thread, sysObjects.Tagable.Tagable):
     async def connectToServer(self):
         """
         """
-        try:
-            reader, writer = await asyncio.open_connection(self.server[0], self.server[1])
-        except ConnectionRefusedError:
-            # print("connect refused. ending...")  # debug
-            return
+        connected = False
+        while not connected:
+            try:
+                reader, writer = await asyncio.open_connection(host=self.server[0], port=self.server[1])
+                connected = True
+                print("client connected")  # debug
+            except ConnectionRefusedError:
+                print("connect refused. ending...")  # debug
+                await asyncio.sleep(0)
         cliId = base64.b64encode(self.tags["id"].encode())
         address = base64.b64encode(self.address.encode())
         port = base64.b64encode(str(self.port).encode())
@@ -102,9 +90,72 @@ class SysClient(threading.Thread, sysObjects.Tagable.Tagable):
 
         fullB = connectB + objB
         writer.write(fullB)
+        print("client: registration sent")  # debug
         try:
             await writer.drain()
         except ConnectionResetError:
-            # print("connection reset. ending...")  # debug
+            print("connection reset. ending...")  # debug
             return
-        await asyncio.gather(self.sendShifts(writer), self.handleServerRequests(reader))
+        await asyncio.gather(self.sendShifts(writer), self.handleServer(reader, writer))
+
+    async def sendShifts(self, writer):
+        while self.live and not writer.is_closing():  # Exit on cli death or writer close.
+            if self.shiftQueue:  # Only try to send a shift if there are any to send.
+                shift, idx = self.shiftQueue.pop()
+                shiftDt = base64.b64encode(pickle.dumps(shift))
+                idxDt = base64.b64encode(str(idx).encode())
+                objIdDt = base64.b64encode(self.obj[2].tags["id"].encode())
+                writer.write(b"shift:" + objIdDt + b":" + shiftDt + b":" + idxDt + b":end")
+                try:
+                    await writer.drain()
+                except ConnectionResetError:
+                    print("server connection reset. ending...")  # debug
+                    return
+            else:
+                await asyncio.sleep(0)
+
+    async def handleServer(self, reader, writer):
+        connected = True
+        lastTime = time.monotonic()
+        while connected:
+            try:
+                dataType = (await reader.readuntil(b":"))[:-1]  # Separate data type header.
+                data = (await reader.readuntil(b":end"))[:-4]  # Remove footer.
+            except asyncio.IncompleteReadError:
+                print("Server Disconnected Unexpectedly")  # debug
+                return
+            except ConnectionResetError:
+                print("Server Disconnected Unexpectedly")  # debug
+                return
+
+            if time.monotonic() >= (lastTime + (60 * 5)):  # DC for timeout
+                break
+
+            if dataType == b"obj":  # obj:<object>:end
+                await self.receiveObject(data)
+                lastTime = time.monotonic()  # Store time object was received.
+                print("client: got obj update")  # debug
+            elif dataType == b"disconnect":  # disconnect::end
+                print("client: got disconnect")  # debug
+                break
+
+        await writer.drain()  # cleanup for disconnect.
+        writer.write(b"disconnect::end")
+        await writer.drain()
+        writer.write_eof()
+        writer.close()
+        print("client: disconnecting")  # debug
+        await writer.wait_closed()
+
+    async def receiveObject(self, data):
+        """
+        Update self.obj with the object received from the server encoded in <data>.
+
+        :param bytes data:
+        """
+        obj = pickle.loads(base64.b64decode(data))
+        self.obj[2] = obj
+        # await self.display_update()  # At some point...
+
+    def enqueueShift(self, shift, idx):
+        self.shiftQueue.append((shift, idx))
--- ClientServer/Server.py
+++ ClientServer/Server.py
@@ -48,102 +48,9 @@ class SysServer(threading.Thread, sysObjects.Tagable.Tagable):
         sockSrv = await asyncio.start_server(self.handleClient, port=8426)
         try:
             await asyncio.gather(sockSrv.serve_forever(), self.taskManager())
+            print("server Ready")
         except asyncio.CancelledError:
             print("Tasks canceled. Live: {}".format(self.live))  # debug
-
-    async def sendNetworkedObjects(self):
-        """
-
-        :return: Networked objects.
-        :rtype: list
-        """
-        while self.live:
-            netObjs = []
-            await asyncio.sleep(0)
-            for dirr in self.directoryList:  # run though all directories and allow execution to move.
-                for ses in dirr.sessionList:  # run though all sessions in dirr and allow execution to move.
-                    for obj in ses.objectList:  # run though all objects in ses but be blocking at this part.
-                        if "networkObject" in obj.tags.keys():
-                            netObjs.append(obj)
-            await asyncio.gather(*(self.sendNetworkObject(obj) for obj in netObjs))
-
-    async def sendNetworkObject(self, obj):
-        """
-
-        :param Tagable obj:
-        """
-        try:
-            address, port = self.clientLookupTable[
-                obj.tags["networkObject"]]  # Lookup the address and port of the client.
-            writer = (await asyncio.open_connection(address, port))[1]  # Open a connection and get the writer.
-        except ConnectionRefusedError:
-            # print("connect refused. ending...")  # debug
-            return
-        except KeyError:
-            # print("client not found. ending...")  # debug
-            return
-        objDt = base64.b64encode(pickle.dumps(obj))
-        writer.write(b"obj:" + objDt + b":end")
-        try:
-            await writer.drain()
-        except ConnectionResetError:
-            # print("connection reset. ending...")  # debug
-            return
-        writer.close()
-        await writer.wait_closed()
-
-    async def handleClient(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        """
-        Read and handle data from the client.
-
-        :param StreamReader reader: Data from client to read.
-        :param StreamWriter writer: Unused
-        """
-        connected = True
-        lastMessage = time.monotonic()
-        while connected:
-            if reader.at_eof():
-                if time.monotonic() >= (
-                        lastMessage + (60 * 5)):  # If its been five minutes since last message, disconnect.
-                    connected = False
-                await asyncio.sleep(0)  # Pass execution while doing nothing.
-                continue  # Otherwise wait for next message.
-            lastMessage = time.monotonic()  # Store time message was received.
-            dataType = (await reader.readuntil(b":"))[:-1]  # Separate data type header.
-            data = (await reader.readuntil(b":end"))[:-4]  # Remove footer.
-            if dataType == b"obj":  # obj:<dirId>:<sesId>:<object>:end
-                await self.receiveObject(data)
-            elif dataType == b"shift":  # shift:<objectId>:<shift>:<idx>:end
-                await self.receiveShift(data)
-            elif dataType == b"newConnection":  # newConnection:<cliId>:<address>:<port>:end
-                cliId = await self.registerNewClient(data)
-                asyncio.create_task(self.continuousSend(cliId, writer))
-            elif dataType == b"disconnect":  # disconnect::end
-                connected = False
-
-        await writer.drain()  # cleanup for disconnect.
-        writer.write(b"disconnect::end")
-        await writer.drain()
-        writer.write_eof()
-        writer.close()
-        await writer.wait_closed()
-
-    async def registerNewClient(self, data):
-        """
-        Add a new entry to the clientLookupTable.
-
-        :param bytes data: The data from the client.
-            <cliId>:<address>:<port> where cliId, address, and port are base64 encoded.
-        :return: Client Id
-        :rtype: str
-        """
-        data = data.split(b":")  # <cliId>:<address>:<port> -> [cliId, address, port]
-        newData = []
-        for idx in range(data.__len__()):  # Base64 decode and convert to string.
-            newData.append(base64.b64decode(data[idx]).decode())
-        self.clientLookupTable[newData[0]] = (
-            newData[1], int(newData[2]))  # add {<cliId>:(<address>, <port>)} to cliLookupTable.
-        return newData[0]  # return client Id
 
     async def taskManager(self):
         """
@@ -157,6 +64,68 @@ class SysServer(threading.Thread, sysObjects.Tagable.Tagable):
                 task.cancel()
             except asyncio.CancelledError:
                 pass
+
+    async def handleClient(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        """
+        Read and handle data from the client.
+
+        :param StreamReader reader: Data from client to read.
+        :param StreamWriter writer: Unused
+        """
+        connected = True
+        lastTime = time.monotonic()
+        while connected:
+            try:
+                dataType = (await reader.readuntil(b":"))[:-1]  # Separate data type header.
+                data = (await reader.readuntil(b":end"))[:-4]  # Remove footer.
+            except asyncio.IncompleteReadError:
+                print("client disconnected unexpectedly")  # debug
+                return  # Exit due to client disconnect.
+            except ConnectionResetError:
+                print("client disconnected unexpectedly")  # debug
+                return  # Exit due to client disconnect.
+
+            if time.monotonic() >= (lastTime + (60 * 5)):  # DC for timeout
+                break
+
+            if dataType == b"obj":  # obj:<dirId>:<sesId>:<object>:end
+                await self.receiveObject(data)
+                print("server: got object")  # debug
+            elif dataType == b"shift":  # shift:<objectId>:<shift>:<idx>:end
+                await self.receiveShift(data)
+                lastTime = time.monotonic()  # Store time object was received.
+                print("server: got shift")  # debug
+            elif dataType == b"newConnection":  # newConnection:<cliId>:<address>:<port>:end
+                cliId = await self.registerNewClient(data)
+                asyncio.create_task(self.continuousSend(cliId, writer))
+                print("server: got connection")  # debug
+            elif dataType == b"disconnect":  # disconnect::end
+                print("server: got disconnect")  # debug
+                break
+
+        await writer.drain()  # cleanup for disconnect.
+        writer.write(b"disconnect::end")
+        await writer.drain()
+        writer.write_eof()
+        writer.close()
+        print("server: disconnecting")  # debug
+        await writer.wait_closed()
+
+    async def receiveObject(self, data):
+        """
+
+        :param bytes data: <dirId>:<sesId>:<object>
+        """
+        data = data.split(b":")  # <dirId>:<sesId>:<object> -> [<dirId>, <sesId>, <object>]
+        dirId = base64.b64decode(data[0]).decode()
+        sesId = base64.b64decode(data[1]).decode()
+        obj = pickle.loads(base64.b64decode(data[2]))
+        for dirr in self.directoryList:
+            if dirr.tags["id"] == dirId:
+                for ses in dirr.sessionList:
+                    if ses.tags["id"] == sesId:
+                        ses.addObject(obj)
+                        return
 
     async def receiveShift(self, data):
         """
@@ -185,23 +154,24 @@ class SysServer(threading.Thread, sysObjects.Tagable.Tagable):
             try:
                 o.tasker.shifts.insert(int(idx))
             except ValueError:
-                pass  # ???
+                pass  # this should never happen but if it does ignore the shift.
 
-    async def receiveObject(self, data):
+    async def registerNewClient(self, data):
         """
+        Add a new entry to the clientLookupTable.
 
-        :param bytes data: <dirId>:<sesId>:<object>
+        :param bytes data: The data from the client.
+            <cliId>:<address>:<port> where cliId, address, and port are base64 encoded.
+        :return: Client Id
+        :rtype: str
         """
-        data = data.split(b":")  # <dirId>:<sesId>:<object> -> [<dirId>, <sesId>, <object>]
-        dirId = base64.b64decode(data[0]).decode()
-        sesId = base64.b64decode(data[1]).decode()
-        obj = pickle.loads(base64.b64decode(data[2]))
-        for dirr in self.directoryList:
-            if dirr.tags["id"] == dirId:
-                for ses in dirr.sessionList:
-                    if ses.tags["id"] == sesId:
-                        ses.addObject(obj)
-                        return
+        data = data.split(b":")  # <cliId>:<address>:<port> -> [cliId, address, port]
+        newData = []
+        for idx in range(data.__len__()):  # Base64 decode and convert to string.
+            newData.append(base64.b64decode(data[idx]).decode())
+        self.clientLookupTable[newData[0]] = (
+            newData[1], int(newData[2]))  # add {<cliId>:(<address>, <port>)} to cliLookupTable.
+        return newData[0]  # return client Id
 
     async def continuousSend(self, cliId, writer):
         """
@@ -211,6 +181,7 @@ class SysServer(threading.Thread, sysObjects.Tagable.Tagable):
         """
         cliObj = None
         while self.live and not writer.is_closing():
+            await asyncio.sleep(4)
             if cliObj is None or ("networkObject" not in cliObj.tags.keys()) or cliObj.tags["networkObject"] != cliId:
                 # acquire the networkObject
                 cliObj = None
@@ -225,10 +196,13 @@ class SysServer(threading.Thread, sysObjects.Tagable.Tagable):
                             break
                     if cliObj is not None:  # Exit loop on acquisition
                         break
-            objDt = base64.b64encode(pickle.dumps(cliObj))
-            writer.write(b"obj:" + objDt + b":end")
-            try:
-                await writer.drain()
-            except ConnectionResetError:
-                # print("connection reset. ending...")  # debug
-                return
+            if cliObj is not None:
+                objDt = base64.b64encode(pickle.dumps(cliObj))
+                try:
+                    writer.write(b"obj:" + objDt + b":end")
+                    await writer.drain()
+                except ConnectionResetError:
+                    print("client connection reset. ending...")  # debug
+                    return
+                except RuntimeError:
+                    print("writer from client closed. ending...")  # debug
